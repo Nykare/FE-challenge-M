6230ed864052048f421fb5d1345ad344f54228e1
Niki Veček <nikky.ca@gmail.com>
2024-12-10 10:31:28 +0100

    Update About.svelte

--- src/About.svelte
+++ src/About.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-3xl md:text-6xl leading-[1] font-black dark:text-white py-[10px] md:py-[20px] px-[20px]">About <span class="inline-block animate-wiggle">🤖</span></h1>
+<h1 class="text-3xl md:text-6xl leading-[1] font-black text-black dark:text-white py-[10px] md:py-[20px] px-[20px]">About <span class="inline-block animate-wiggle">🤖</span></h1>
 <p class="px-[20px] py-[0] leading-snug text-p-light dark:text-p-dark">Learn a little bit about us</p>
