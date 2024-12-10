47a4a403ffddb1737d96470abd0c02548019335f
Niki Veček <nikky.ca@gmail.com>
2024-12-10 10:32:27 +0100

    Update Services.svelte

--- src/Services.svelte
+++ src/Services.svelte
@@ -2,7 +2,7 @@
 	import { navItems } from "./navItems.ts";
 </script>
 
-<h1 class="text-3xl md:text-6xl leading-[1] font-black dark:text-white py-[10px] md:py-[20px] px-[20px]">Services <span class="inline-block animate-wiggle">⚙️</span></h1>
+<h1 class="text-3xl md:text-6xl leading-[1] font-black text-black dark:text-white py-[10px] md:py-[20px] px-[20px]">Services <span class="inline-block animate-wiggle">⚙️</span></h1>
 <p class="px-[20px] py-[0] leading-snug text-p-light dark:text-p-dark">A list of our services</p>
 
 {#each navItems as item}
