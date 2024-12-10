d5dbd98e4c81b56933e5ea31e0c7e9623ba8d4db
Niki Veček <nikky.ca@gmail.com>
2024-12-10 10:32:08 +0100

    Update ServiceDesign.svelte

--- src/ServiceDesign.svelte
+++ src/ServiceDesign.svelte
@@ -1,3 +1,3 @@
-<h1 class="text-3xl md:text-6xl leading-[1] font-black dark:text-white py-[10px] md:py-[20px] px-[20px]">Design <span class="inline-block animate-wiggle">🎨</span></h1>
+<h1 class="text-3xl md:text-6xl leading-[1] font-black text-black dark:text-white py-[10px] md:py-[20px] px-[20px]">Design <span class="inline-block animate-wiggle">🎨</span></h1>
 <p class="px-[20px] py-[0] leading-snug text-p-light dark:text-p-dark">Learn more about Design</p>
 <a href="/#/services" title="Services" class="table px-[18px] py-[12px] text-xs leading-tight font-medium text-p-light dark:text-p-dark hover:text-pink text-left border border-light-gray dark:border-dark-gray rounded-[10px] ml-[auto] mr-[auto] mt-[20px]">← &nbsp; Services</a>
