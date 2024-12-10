4cdc6de0d97633c2036c67c0557dcb5757a17ea1
Niki Veček <nikky.ca@gmail.com>
2024-12-10 10:33:09 +0100

    Update ThemeToggle.svelte

--- src/ThemeToggle.svelte
+++ src/ThemeToggle.svelte
@@ -11,6 +11,6 @@
 		document.documentElement.classList.add('dark');
 	}
 </script>
-<button class="fixed z-40 bottom-[15px] right-[15px] bg-white dark:bg-gray-overlay shadow-[0px_10px_40px_rgba(0,0,0,0.06)] dark:shadow-[0] rounded-[40px] w-[80px] p-[5px]" on:click={toggleDarkMode} aria-label="Toggle">
+<button class="fixed z-40 bottom-[15px] right-[15px] bg-white dark:bg-gray-overlay shadow-[0px_10px_40px_rgba(0,0,0,0.06)] dark:shadow-none rounded-[40px] w-[80px] p-[5px]" on:click={toggleDarkMode} aria-label="Toggle">
 	<div class="w-[40px] h-[40px] rounded-[40px] bg-white dark:bg-gray-overlay border border-light-gray dark:border-dark-gray transition-margin linear ml-[0px] dark:ml-[30px]"><div class="bg-[url('./assets/theme-toggle.svg')] w-[40px] h-[40px] bg-left-top dark:bg-right-top"></div></div>
 </button>
